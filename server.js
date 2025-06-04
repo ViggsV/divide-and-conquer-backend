2317186dfbeb1101bf6436b81dac4e4471b309bb
Mnyoni <mnyoni@yahoo.co.uk>
2025-06-04 09:20:56 +0100

    resolve pull conflicts"

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = 3001;
+const cors = require("cors");
+require("dotenv").config();
 
-// Middleware
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.1:3000"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB:", err));
 
-<<<<<<< HEAD
-// Routes
-const adRoutes = require('./routes/adRoutes');
-const authRoutes = require('./routes/authRoutes');
-=======
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
->>>>>>> 23909bcddfe4d83ec6062aba98606e548f1b1dbd
 
-app.use('/chores', choreRoutes);
-app.use('/auth', authRoutes);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
 });
