913a62ab3693744efb700a91ce6a0d787f6fa67e
Mnyoni <mnyoni@yahoo.co.uk>
2025-06-05 19:00:07 +0100

    server and controllers updated

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 
+const choreRoutes = require("./routes/choreRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
+
+
+app.use("/chores", choreRoutes);
 app.use("/auth", authRoutes);
 
 app.listen(port, () => {
